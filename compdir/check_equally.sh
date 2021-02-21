a7f07ab39ce0a1c96cdcb500c039c74f7a47adf0
unaguna <k.izumi.ysk@gmail.com>
2021-02-21 11:37:40 +0900

    ♻ハッシュリストの作成と比較を分離

--- compdir/check_equally.sh
+++ compdir/check_equally.sh
@@ -18,6 +18,11 @@ function hashlist () {
     return $?
 }
 
+function comp_hashlist () {
+    ./comp_hashlist.sh "$@"
+    return $?
+}
+
 SHELL_NAME=`basename $0`
 TMP_DIR="/tmp"
 
@@ -32,7 +37,7 @@ function finally () {
     set +e +o pipefail
     
     # 一時ファイルが存在する場合に削除
-    rm -f $base_list_tmp $target_list $inter_base_list $inter_target_list
+    rm -f $base_list_tmp $target_list
 }
 trap finally EXIT
 
@@ -67,15 +72,8 @@ else
     target_dir=${2:-"."}
 fi
 
-TAG_BASE=${TAG_BASE:-"base"}
-TAG_BASE_EMPTY=`echo $TAG_BASE | sed 's/./-/g'`
-TAG_TARGET=${TAG_TARGET:-"target"}
-TAG_TARGET_EMPTY=`echo $TAG_TARGET | sed 's/./-/g'`
-
 base_list_tmp=`mktemp $TMP_DIR/$SHELL_NAME.base_list.XXXXXX`
 target_list=`mktemp $TMP_DIR/$SHELL_NAME.target_list.XXXXXX`
-inter_base_list=`mktemp $TMP_DIR/$SHELL_NAME.inter_base_list.XXXXXX`
-inter_target_list=`mktemp $TMP_DIR/$SHELL_NAME.inter_target_list.XXXXXX`
 
 
 # すべてのファイルは次のいずれかに当てはまる。
@@ -95,35 +93,5 @@ fi
 # target のハッシュリストを作成。
 hashlist $target_dir > $target_list
 
-# 2つのハッシュリストを加工し、先頭に『存在フラグ』フィールドを付ける。
-cat $base_list | awk "{print \"1 \"\$1\" \"\$2}" > $inter_base_list
-cat $target_list | awk "{print \"2 \"\$1\" \"\$2}" > $inter_target_list
 
-# 2つのハッシュリストを以下の手順で比較する。
-# cat $ $
-#   2つのハッシュリストを連結する。
-#   この時点で、各ファイルについて次の状態になる。
-#         (I). 『存在フラグ』が1の行だけある。
-#        (II). 『存在フラグ』が2の行だけある。
-#       (III). 『存在フラグ』が1の行と2の行があり、『ハッシュ値』は一致しない
-#        (IV). 『存在フラグ』が1の行と2の行があり、『ハッシュ値』は一致する
-# sort -k 3 | uniq -f1 -u
-#   (ハッシュ値, ファイルパス) の組が重複する場合、このファイルは base と target で一致しているので除外する。
-#   この時点で、各ファイルについて次の状態になる。
-#         (I). 『存在フラグ』が1の行だけある。
-#        (II). 『存在フラグ』が2の行だけある。
-#       (III). 『存在フラグ』が1の行と2の行があり、『ハッシュ値』は一致しない
-#        (IV). 行がない
-# awk '{arr[$3]+=$1} END{for(i in arr) print arr[i], i}'
-#   SQLでいうところの「SELECT sum(存在フラグ),ファイルパス GROUP BY ファイルパス」
-#   これにより、すべての行は次のいずれかに当てはまる。
-#         (I). 『存在フラグ』が1の行だけある。
-#        (II). 『存在フラグ』が2の行だけある。
-#       (III). 『存在フラグ』が3の行だけある。
-#        (IV). 行がない
-# sed -e ...
-#   上記の『存在フラグ』を、視認しやすい文字列へ変換する。
-cat $inter_base_list $inter_target_list | \
-sort -k 3 | uniq -f1 -u | \
-awk '{arr[$3]+=$1} END{for(i in arr) print arr[i], i}' | \
-sed -e "s/^1/$TAG_BASE -- $TAG_TARGET_EMPTY/" -e "s/^2/$TAG_BASE_EMPTY -- $TAG_TARGET/" -e "s/^3/$TAG_BASE != $TAG_TARGET/"
+comp_hashlist $base_list $target_list
---
+++ compdir/comp_hashlist.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# 2つのディレクトリの内容を比較し、差分をファイル一覧として出力する。
+# ファイル同士の同一性はそのハッシュ値によって判断するため、原理的には誤陰性が起こりうる。
+
+function usage_exit () {
+    echo "Usage:" `basename $0` "<base_hashlist> [<target_hashlist>]"
+    echo
+    echo "Environment Variables:"
+    echo "    TAG_BASE:   比較結果の表示に使用される、比較元ディレクトリを表す文字列。"
+    echo "    TAG_TARGET: 比較結果の表示に使用される、比較対象ディレクトリを表す文字列。"
+    exit
+}
+
+SHELL_NAME=`basename $0`
+TMP_DIR="/tmp"
+
+
+################################################################################
+# エラーハンドリング
+################################################################################
+
+set -e -o pipefail
+
+function finally () {
+    set +e +o pipefail
+    
+    # 一時ファイルが存在する場合に削除
+    rm -f $inter_base_list $inter_target_list
+}
+trap finally EXIT
+
+################################################################################
+# 引数取得
+################################################################################
+
+if [ $# -le 0 -o $# -ge 3 ]; then
+    usage_exit
+fi
+
+base_list=$1
+target_list=${2:-"-"}
+
+TAG_BASE=${TAG_BASE:-"base"}
+TAG_BASE_EMPTY=`echo $TAG_BASE | sed 's/./-/g'`
+TAG_TARGET=${TAG_TARGET:-"target"}
+TAG_TARGET_EMPTY=`echo $TAG_TARGET | sed 's/./-/g'`
+
+inter_base_list=`mktemp $TMP_DIR/$SHELL_NAME.inter_base_list.XXXXXX`
+inter_target_list=`mktemp $TMP_DIR/$SHELL_NAME.inter_target_list.XXXXXX`
+
+
+# すべてのファイルは次のいずれかに当てはまる。
+#   (I). base にのみ存在し、target には存在しない
+#   (II). target にのみ存在し、base には存在しない
+#   (III). base にも target にも存在するが内容が一致しない
+#   (IV). base にも target にも存在し、内容が一致する
+# これらのうち、(I),(II),(III) に当てはまるものだけを抽出し、
+# 各ファイルがどれに当てはまるかがわかる形式で出力する。
+
+
+# 2つのハッシュリストを加工し、先頭に『存在フラグ』フィールドを付ける。
+cat $base_list | awk "{print \"1 \"\$1\" \"\$2}" > $inter_base_list
+cat $target_list | awk "{print \"2 \"\$1\" \"\$2}" > $inter_target_list
+
+# 2つのハッシュリストを以下の手順で比較する。
+# cat $ $
+#   2つのハッシュリストを連結する。
+#   この時点で、各ファイルについて次の状態になる。
+#         (I). 『存在フラグ』が1の行だけある。
+#        (II). 『存在フラグ』が2の行だけある。
+#       (III). 『存在フラグ』が1の行と2の行があり、『ハッシュ値』は一致しない
+#        (IV). 『存在フラグ』が1の行と2の行があり、『ハッシュ値』は一致する
+# sort -k 3 | uniq -f1 -u
+#   (ハッシュ値, ファイルパス) の組が重複する場合、このファイルは base と target で一致しているので除外する。
+#   この時点で、各ファイルについて次の状態になる。
+#         (I). 『存在フラグ』が1の行だけある。
+#        (II). 『存在フラグ』が2の行だけある。
+#       (III). 『存在フラグ』が1の行と2の行があり、『ハッシュ値』は一致しない
+#        (IV). 行がない
+# awk '{arr[$3]+=$1} END{for(i in arr) print arr[i], i}'
+#   SQLでいうところの「SELECT sum(存在フラグ),ファイルパス GROUP BY ファイルパス」
+#   これにより、すべての行は次のいずれかに当てはまる。
+#         (I). 『存在フラグ』が1の行だけある。
+#        (II). 『存在フラグ』が2の行だけある。
+#       (III). 『存在フラグ』が3の行だけある。
+#        (IV). 行がない
+# sed -e ...
+#   上記の『存在フラグ』を、視認しやすい文字列へ変換する。
+cat $inter_base_list $inter_target_list | \
+sort -k 3 | uniq -f1 -u | \
+awk '{arr[$3]+=$1} END{for(i in arr) print arr[i], i}' | \
+sed -e "s/^1/$TAG_BASE -- $TAG_TARGET_EMPTY/" -e "s/^2/$TAG_BASE_EMPTY -- $TAG_TARGET/" -e "s/^3/$TAG_BASE != $TAG_TARGET/"
