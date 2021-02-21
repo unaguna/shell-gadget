2342b3decf0caacabf1645608755ad0af10ebea0
unaguna <k.izumi.ysk@gmail.com>
2021-02-21 21:49:59 +0900

    ♻一時ファイルの作成を削減

--- compdir/comp_hashlist.sh
+++ compdir/comp_hashlist.sh
@@ -13,7 +13,6 @@ function usage_exit () {
 }
 
 SHELL_NAME=`basename $0`
-TMP_DIR="/tmp"
 
 
 ################################################################################
@@ -21,14 +20,6 @@ TMP_DIR="/tmp"
 ################################################################################
 
 set -e -o pipefail
-
-function finally () {
-    set +e +o pipefail
-    
-    # 一時ファイルが存在する場合に削除
-    rm -f $inter_hashlist
-}
-trap finally EXIT
 
 ################################################################################
 # 引数取得
@@ -46,8 +37,6 @@ TAG_BASE_EMPTY=`echo $TAG_BASE | sed 's/./-/g'`
 TAG_TARGET=${TAG_TARGET:-"target"}
 TAG_TARGET_EMPTY=`echo $TAG_TARGET | sed 's/./-/g'`
 
-inter_hashlist=`mktemp $TMP_DIR/$SHELL_NAME.inter_hashlist.XXXXXX`
-
 
 # すべてのファイルは次のいずれかに当てはまる。
 #   (I). base にのみ存在し、target には存在しない
@@ -58,18 +47,15 @@ inter_hashlist=`mktemp $TMP_DIR/$SHELL_NAME.inter_hashlist.XXXXXX`
 # 各ファイルがどれに当てはまるかがわかる形式で出力する。
 
 
-# 2つのハッシュリストを加工し結合。
-# 先頭に『存在フラグ』フィールドを付ける。
+# 2つのハッシュリストを比較する。
+# { awk ... awk ... }
+#   2つのハッシュリストを加工し結合。
+#   先頭に『存在フラグ』フィールドを付ける。
 #   この時点で、各ファイルについて次の状態になる。
 #         (I). 『存在フラグ』が1の行だけある。
 #        (II). 『存在フラグ』が2の行だけある。
 #       (III). 『存在フラグ』が1の行と2の行があり、『ハッシュ値』は一致しない
 #        (IV). 『存在フラグ』が1の行と2の行があり、『ハッシュ値』は一致する
-cat $base_list | awk "{print \"1 \"\$1\" \"\$2}" > $inter_hashlist
-cat $target_list | awk "{print \"2 \"\$1\" \"\$2}" >> $inter_hashlist
-
-
-# inter_hashlist を以下の手順で加工することで、2つのハッシュリストを比較する。
 # sort -k 3 | uniq -f1 -u
 #   (ハッシュ値, ファイルパス) の組が重複する場合、このファイルは base と target で一致しているので除外する。
 #   この時点で、各ファイルについて次の状態になる。
@@ -79,13 +65,17 @@ cat $target_list | awk "{print \"2 \"\$1\" \"\$2}" >> $inter_hashlist
 #        (IV). 行がない
 # awk '{arr[$3]+=$1} END{for(i in arr) print arr[i], i}'
 #   SQLでいうところの「SELECT sum(存在フラグ),ファイルパス GROUP BY ファイルパス」
-#   これにより、すべての行は次のいずれかに当てはまる。
+#   この時点で、各ファイルについて次の状態になる。
 #         (I). 『存在フラグ』が1の行だけある。
 #        (II). 『存在フラグ』が2の行だけある。
 #       (III). 『存在フラグ』が3の行だけある。
 #        (IV). 行がない
 # sed -e ...
 #   上記の『存在フラグ』を、視認しやすい文字列へ変換する。
-sort -k 3 $inter_hashlist | uniq -f1 -u | \
+{
+    awk "{print \"1 \"\$1\" \"\$2}" $base_list
+    awk "{print \"2 \"\$1\" \"\$2}" $target_list
+} | \
+sort -k 3 | uniq -f1 -u | \
 awk '{arr[$3]+=$1} END{for(i in arr) print arr[i], i}' | \
 sed -e "s/^1/$TAG_BASE -- $TAG_TARGET_EMPTY/" -e "s/^2/$TAG_BASE_EMPTY -- $TAG_TARGET/" -e "s/^3/$TAG_BASE != $TAG_TARGET/"
