b41b59b3911486d534145f75b57053aaef6eead7
unaguna <k.izumi.ysk@gmail.com>
2021-03-07 22:30:41 +0900

    ✨getip4listでもgetip4のオプションが使えるよう修正

--- getip/getip4list.sh
+++ getip/getip4list.sh
@@ -25,6 +25,52 @@ trap finally EXIT
 
 
 ################################################################################
+# 引数取得
+################################################################################
+
+# 渡す引数リストを args として生成する。
+options=()
+args=()
+while (( $# > 0 ))
+do
+    use_opt_arg=
+    if [[ "$1" =~ ^- ]]; then
+        if [[ "$1" =~ "T" ]]; then
+            options+=( "-T" )
+        fi
+        if [[ "$1" =~ "w" ]]; then
+            options+=( "-w" )
+        fi
+        if [[ "$1" =~ "s" ]]; then
+            options+=( "-s" )
+        fi
+        if [[ "$1" =~ "r" ]]; then
+            options+=( "-r" )
+        fi
+        if [[ "$1" =~ "W" ]]; then
+            options+=( "-W" )
+            options+=( "$2" )
+            use_opt_arg=true
+        fi
+        if [[ "$1" =~ "R" ]]; then
+            options+=( "-R" )
+            options+=( "$2" )
+            use_opt_arg=true
+        fi
+    else
+        args+=( "$1" )
+    fi
+
+
+    if [ -n "$use_opt_arg" ]; then
+        shift 2
+    else
+        shift
+    fi
+done
+
+
+################################################################################
 # メイン処理
 ################################################################################
 
@@ -35,11 +81,8 @@ trap finally EXIT
 someone_errored=
 
 # 各引数について getip4.sh を実行する。
-while (( $# > 0 )); do
-    host_name="$1"
-    shift
-
-    ip_address=`getip4.sh "$host_name" | head -n1`
+for host_name in "${args[@]}"; do
+    ip_address=`getip4.sh "${options[@]}" "$host_name" | head -n1`
     exit_code=$?
     if [ $exit_code -ne 0 ]; then
         someone_errored=true
